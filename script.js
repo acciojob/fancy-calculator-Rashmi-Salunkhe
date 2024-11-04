9ad559718b89c09523b659541d512180f814e16f
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-11-04 19:10:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let first=prompt()
+let firstint=parseInt(first)
+let second=prompt()
+let secondint=parseInt(second)
+let result=firstint*2+secondint
+
+alert("The final result is: " + result)
